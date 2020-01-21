1d1eb85d4c79301361f59c31df9841d6f136c893
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-21 11:39:18 +0000

    Setting up firestore

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { vuexfireMutations } from 'vuexfire'
+
+import auth from './modules/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  modules: {
+    auth
   },
-  mutations: {
+  state() {
+    return {}
+  },
+  getters: {
+
   },
   actions: {
   },
-  modules: {
-  }
+  mutations: {
+    ...vuexfireMutations
+  },
 })
