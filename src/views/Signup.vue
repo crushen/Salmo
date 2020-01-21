51f568d9cdf889d00cc85aedee10ea20515ec921
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-21 11:53:39 +0000

    Signup template

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,3 +1,76 @@
 <template>
-  <h1>Signup</h1>
+  <section id="register">
+    <h1>Signup</h1>
+    <form>
+      <div class="field">
+        <input 
+          v-model="form.firstName"
+          type="text"
+          placeholder="First Name">
+      </div>
+      <div class="field">
+        <input 
+          v-model="form.lastName"
+          type="text"
+          placeholder="Last Name">
+      </div>
+      <div class="field">
+        <input 
+          v-model="form.email"
+          type="email"
+          placeholder="Email"
+          autocomplete="email">
+      </div>
+      <div class="field">
+        <input 
+          v-model="form.password"
+          type="password"
+          placeholder="Password"
+          autocomplete="current-password">
+      </div>
+      <div class="field">
+        <input 
+          v-model="form.confirmPassword"
+          type="password"
+          placeholder="Confirm Password">
+      </div>
+      <button 
+        @click="handleRegister"
+        type="button">
+        Sign Up
+      </button>
+    </form>
+  </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  width: 300px;
+  margin: auto;
+}
+
+.field {
+  width: 90%;
+  margin: auto;
+}
+
+input {
+  width: 100%;
+}
+</style>
