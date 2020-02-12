a7e7e27d5b337986c9a06a930a830e57f902c2c5
Charlotte Rushen <charlotterushen@hotmail.com>
2020-02-12 15:59:53 +0000

    Added date picker

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import Vuelidate from 'vuelidate';
+import VCalendar from 'v-calendar';
 
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
 Vue.config.productionTip = false;
 Vue.use(Vuelidate);
+Vue.use(VCalendar);
 
 let app;
 firebase.auth().onAuthStateChanged(async user => {
