d368260e03767e1506f77a729712ebae41fbc113
Charlotte Rushen <charlotterushen@hotmail.com>
2020-02-12 13:05:50 +0000

    Added country and DOB question components

---
+++ src/components/questionnaire/AskCountry.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <label>What is your country of origin?</label> 
+
+    <div>
+      <select 
+        v-model="country"
+        name="Country">
+        <option
+          v-for="country in countries"
+          :key="country"
+          :value="country">
+          {{ country }}
+        </option>
+      </select>
+    </div>
+
+    <button
+      @click="askDOB">
+      Next
+    </button> 
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      countries: ['China', 'Taiwan', 'Turkey', 'United States of America', 'United Kingdom'],
+      country: ''
+    }
+  },
+  methods: {
+    askDOB() {
+      this.$emit('askDOB', this.country);
+    }
+  }
+}
+</script>
--- src/store/modules/questions.js
+++ src/store/modules/questions.js
@@ -164,6 +164,30 @@ const questions = [
   }
 ]
 
+// const questions = [ //coreQuestions?
+//   {
+//     question: 'What is your country of origin?',
+//     countries: [
+//       {
+//         value: 'China'
+//       },
+//       {
+//         value: 'Turkey'
+//       },
+//       {
+//         value: 'United Kingdom'
+//       }
+//     ]
+//   },
+//   {
+//     question: 'What is your date of birth?',
+//     day: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+//     month: ['Jan', 'Feb', 'March'],
+//     year: [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008]
+    
+//   }
+// ]
+
 export default {
   namespaced: true,
   state: {
