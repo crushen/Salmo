2090a0224c0c2cd3aee3dd86411755b3016cefea
Charlotte Rushen <charlotterushen@hotmail.com>
2020-03-18 16:51:25 +0000

    Visa page - map state to get visa info

---
+++ src/views/visa/VisaPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ visa.name }}</h1>
+    <router-link
+      :to="{ name: 'visa-info' }"
+      tag="button">
+      Back to Visa Info
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  data() {
+    return {
+      slug: this.$route.params.slug
+    }
+  },
+  computed: {
+    ...mapState('visas', ['visaList']),
+    visa() {
+      return this.visaList.find(item => item.slug === this.slug);
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  margin-bottom: 50px;
+}
+</style>
