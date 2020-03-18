2e0c8af314f6871ddbf7f65cc44cca8da828be6e
Charlotte Rushen <charlotterushen@hotmail.com>
2020-03-18 16:50:57 +0000

    Visa info page

---
+++ src/views/visa/VisaInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Visa Info</h1>
+    <div 
+      v-for="visa in visas"
+      :key="visa.name">
+      <router-link :to="{ name: 'visa-page', params: { slug: visa.slug } }">{{ visa.name }}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      visas : this.$store.state.visas.visaList
+    }
+  }
+}
+</script>
