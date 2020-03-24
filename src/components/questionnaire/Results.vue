24847e2f7c413f667fb8aa4fac53492845e3ba3b
Charlotte Rushen <charlotterushen@hotmail.com>
2020-03-24 17:52:07 +0000

    Results now show topResult, switchOptions & others

--- src/components/questionnaire/Results.vue
+++ src/components/questionnaire/Results.vue
@@ -1,14 +1,38 @@
 <template>
   <div>
     <h1>Quiz done</h1>
-
     <div class="results">
+      <p><strong>Top result:</strong></p>
       <router-link 
-        v-for="visa in visaInfo"
+        v-for="visa in topResult"
         :key="visa.name"
+        tag="div"
+        class="result-link"
         :to="{ name: 'visa-page', params: { slug: visa.slug } }">
-        {{ visa.name }}
+        <p>{{ visa.name }}</p>
+        <p>Switch: {{ visa.switch }}</p>
       </router-link>
+
+      <p><strong>Other {{ visaList[0].category }} visas you can switch to:</strong></p>
+      <router-link 
+        v-for="visa in switchVisas"
+        :key="visa.name"
+        tag="div"
+        class="result-link"
+        :to="{ name: 'visa-page', params: { slug: visa.slug } }">
+        <p>{{ visa.name }}</p>
+      </router-link>
+
+      <p><strong>Visas in the {{ visaList[0].category }} category you can't switch to:</strong></p>
+      <router-link 
+        v-for="visa in otherVisas"
+        :key="visa.name"
+        tag="div"
+        class="result-link"
+        :to="{ name: 'visa-page', params: { slug: visa.slug } }">
+        <p>{{ visa.name }}</p>
+      </router-link>
+
       <!-- <p v-if="messages.switch">{{ messages.switch }}</p>
       <p v-if="messages.dependant">{{ messages.dependant }}</p>
       <p v-if="youthMobility">Youth Mobility Visa</p> -->
@@ -27,51 +51,71 @@ export default {
       results: this.$store.state.questions.result.recommendedVisa,
       currentVisa: this.$store.state.auth.user.profile.currentVisa,
       dependants: this.$store.state.auth.user.profile.dependants,
+      switchOptions: [],
+    
       messages: {
-        switch: null,
-        dependant: null
+        switch: '',
+        dependants: ''
       },
       youthMobility: false
     }
   },
   computed: {
     ...mapState('visas', ['visaList']),
-
-    visaInfo() {
+    topResult() {
       return this.visaList.filter(item => this.results.includes(item.name));
+    },
+    switchVisas() {
+      // Filter visa list for user's switch options
+      const switchOptions = this.visaList.filter(item => this.switchOptions.includes(item.name));
+      // Get all visas in same category
+      const sameCategory = switchOptions.filter(item => item.category === this.visaList[0].category);
+      // Remove visa(s) that appear in top result
+      return sameCategory.filter(item => !this.topResult.includes(item));
+    },
+    otherVisas() {
+      // Filter visa list for visas in same category as top result
+      const sameCategory = this.visaList.filter(item => item.category === this.visaList[0].category);
+      // Remove visa(s) that appear in top result
+      const removeDup = sameCategory.filter(item => !this.topResult.includes(item));
+      // Remove visa(s) that appear in switch visas
+      return removeDup.filter(item => !this.switchVisas.includes(item));
     }
   },
   methods: {
     checkYouthMobility() {
-      const YMvisas = ['Tier 2 General Work Visa', 'Tier 5 Charity Worker Visa', 'Tier 5 GOV Authorised Exchange Visa', 'Tier 5 Creative and Sporting Visa', 'Tier 5 Religious Worker Visa', 'Tier 5 Seasonal Worker Visa' ];
       const YMcountries = ['Australia', 'Canada', 'Japan', 'Monaco', 'New Zealand', 'Hong Kong', 'Hong Kong (British national overseas)', 'South Korea', 'Taiwan', 'British overseas citizen', 'British overseas territories citizen', 'British national (overseas)'];
-      let isYMvisa = false;
-      let isYMcountry = false;
-
-      YMvisas.forEach(visa => {
-        if(this.result === visa) {
-          isYMvisa = true;
-        }
-      });
 
       YMcountries.forEach(country => {
-        if(this.userCountry === country) {
-          isYMcountry = true;
+        if(this.userCountry === country &&
+           this.userAge >= 18 && 
+           this.userAge <= 30 &&
+           this.dependants === 'None') {
+           this.youthMobility = true;
         }
       });
-
-      if(isYMvisa && 
-         isYMcountry && 
-         this.userAge >= 18 && 
-         this.userAge <= 30 &&
-         this.dependants === 'None') {
-        this.youthMobility = true;
+    },
+    checkSwitch() {  
+      if(this.currentVisa === 'Tier 4 General Student Visa') {
+        this.switchOptions = ['Startup Visa', 'Tier 1 Investor Visa - 5 years for PR', 'Tier 1 Investor Visa - 3 years for PR', 'Tier 1 Investor Visa - 2 years for PR', 'Tier 2 General Work Visa', 'Tier 2 Sportsperson Visa', 'Tier 2 Minister of Religion Visa', 'Tier 5 GOV Authorised Exchange Visa', 'Family Visa - Apply on the basis of your private life', 'Family Visa - Coming as an adult to be cared for by a relative route', 'Family Visa - Coming as a parent to join your child route', 'Family Visa - Coming to join your partner/spouse route']
       }
+
+      // For now, will only be testing Student specific questionnaire, so all top results will be switchable
+      this.topResult.forEach(visa => {
+        visa.switch = true;
+      })
     }
   },
   created() {
-    // // Check if they qualify for youth mobility
-    // this.checkYouthMobility();
+    // Check if user is eligable for Youth Mobility Visa
+    this.checkYouthMobility();
+    this.checkSwitch();
+    // Set messages in state, and then push to db
+    this.$store.dispatch('questions/sendDbResults', { // Need to work on this - redundant logic with new flow
+      messages: this.messages,
+      youthMobility: this.youthMobility
+    });
+
     // // Check if they can switch from current visa
     // // CHILD
     // if(this.result === 'Tier 4 Child Student Visa') {
@@ -192,20 +236,22 @@ export default {
     //     this.messages.dependant = "You can bring dependants with you on this visa"
     //   }
     // }
-
-    // Set messages in state, and then push to db
-    this.$store.dispatch('questions/sendDbResults', {
-      messages: this.messages,
-      youthMobility: this.youthMobility
-    });
   }
 }
 </script>
 
 <style scoped>
 .results {
-  margin-top: 30px;
   display: flex;
   flex-direction: column;
+  width: 60%;
+  margin: 30px auto 0 auto;
+}
+
+.result-link {
+  border: 1px solid pink;
+  padding: 10px;
+  margin-bottom: 15px;
+  cursor: pointer;
 }
 </style>
--- src/store/modules/visas.js
+++ src/store/modules/visas.js
@@ -1,28 +1,96 @@
 const visas = [
+  // Switchable from T4 General Student
   {
-    name: 'Tier 4 General Student Visa',
-    slug: 'T4-general-student'
+    name: 'Startup Visa',
+    category: 'business',
+    slug: 'startup'
+  },
+  {
+    name: 'Tier 1 Investor Visa - 5 years for PR',
+    category: 'business',
+    slug: 'T1-investor'
+  },
+  {
+    name: 'Tier 1 Investor Visa - 3 years for PR',
+    category: 'business',
+    slug: 'T1-investor'
+  },
+  {
+    name: 'Tier 1 Investor Visa - 2 years for PR',
+    category: 'business',
+    slug: 'T1-investor'
   },
   {
     name: 'Tier 2 General Work Visa',
+    category: 'work',
     slug: 'T2-general-work'
   },
   {
-    name: 'Tier 4 Short Term Study Visa',
-    slug: 'T4-short-term-study'
-  },
-  {
-    name: 'Family Visa - Apply on the basis of your private life',
-    slug: 'family-visa'
-  },
-  {
     name: 'Tier 2 Sportsperson Visa',
+    category: 'work',
     slug: 'T2-sportsperson'
   },
   {
-    name: 'Tier 5 Creative and Sporting Visa',
-    slug: 'T5-creative-and-sporting'
+    name: 'Tier 2 Minister of Religion Visa',
+    category: 'work',
+    slug: 'T2-minister-of-religion'
+  },
+  {
+    name: 'Tier 5 GOV Authorised Exchange Visa',
+    category: 'work',
+    slug: 'T5-gov-authorised-exchange'
+  },
+  {
+    name: 'Family Visa - Apply on the basis of your private life',
+    category: 'family',
+    slug: 'family-visa'
+  },
+  {
+    name: 'Family Visa - Coming as an adult to be cared for by a relative route',
+    category: 'family',
+    slug: 'family-visa'
+  },
+  {
+    name: 'Family Visa - Coming as a parent to join your child route',
+    category: 'family',
+    slug: 'family-visa'
+  },
+  {
+    name: 'Family Visa - Coming to join your partner/spouse route',
+    category: 'family',
+    slug: 'family-visa'
+  },
+  // Not switchable
+  {
+    name: 'Innovator Visa',
+    category: 'business',
+    slug: 'innovator-visa'
+  },
+  {
+    name: 'Global Talent Visa',
+    category: 'business',
+    slug: 'global-talent-visa'
   }
+
+
+  // {
+  //   name: 'Tier 4 General Student Visa',
+  //   slug: 'T4-general-student'
+  // },
+
+  // {
+  //   name: 'Tier 4 Short Term Study Visa',
+  //   slug: 'T4-short-term-study'
+  // },
+  // {
+  //   name: 'Family Visa - Apply on the basis of your private life',
+  //   slug: 'family-visa'
+  // },
+
+  // {
+  //   name: 'Tier 5 Creative and Sporting Visa',
+  //   slug: 'T5-creative-and-sporting'
+  // }
 ]
 
 export default {
