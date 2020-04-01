6a94b9f8fae2d231eab8f6498287cf258a1a58ee
Charlotte Rushen <charlotterushen@hotmail.com>
2020-04-01 12:07:03 +0100

    Page link cards

---
+++ src/components/PageLinkCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <div class="text">
+      <p>{{ text }}</p>
+    </div>
+    <div class="icon">
+      <!-- <img src="icon" alt=""> -->
+      <h3 v-html="pageTitle" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: { required: true, type: String },
+    pageTitle: { required: true, type: String }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+
+.card {
+  height: 34vw;
+  max-height: 120px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: $spacing*8;
+
+  & .text {
+    width: 55%;
+  }
+
+  & .icon {
+    width: 40%;
+    max-width: 120px;
+    display: flex;
+    align-items: flex-end;
+    padding: $spacing;
+    border-radius: 10px;
+    background: $dark-font;
+    color: $light-font;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,26 +8,53 @@
       <h2 class="caption">The ultimate visa organisation tool.</h2>
     </header>
 
-    <section class="welcome">
+    <section 
+      v-if="user"
+      class="welcome">
       <h2 class="header">Welcome back, {{ user.profile.firstName }}.</h2>
       <div class="placeholder-image"></div>
     </section>
 
     <news />
 
+    <section class="section-margin">
+      <page-link-card
+        v-for="card in pageLinks"
+        :key="card.pageTitle"
+        :text="card.text"
+        :pageTitle="card.pageTitle" />
+    </section>
+
+
   </section>
 </template>
 
 <script>
 import news from '@/components/news/News';
+import pageLinkCard from '@/components/PageLinkCard';
 
 export default {
   components: {
-    news
+    news,
+    pageLinkCard
   },
   data() {
     return {
-      user: this.$store.state.auth.user
+      user: this.$store.state.auth.user,
+      pageLinks: [
+        {
+          text: 'Head over to your results page to see and overview of which visa suits you best.',
+          pageTitle: 'Quiz<br>Results'
+          //link: ''
+          //icon: ''
+        },
+        {
+          text: 'For more general advice or help on the UK aplication process, take a look through our Help Centre.',
+          pageTitle: 'Help<br>Centre'
+          //link: ''
+          //icon: ''
+        }
+      ] 
     }
   }
 }
