5bcb2342b8e20de0e7f63cb3c4f6b3801116267d
Charlotte Rushen <charlotterushen@hotmail.com>
2020-04-08 15:45:44 +0100

    Visa info page

--- src/views/visa/VisaInfo.vue
+++ src/views/visa/VisaInfo.vue
@@ -1,10 +1,21 @@
 <template>
   <section class="content">
-    <h1>Visa Info</h1>
+    <!-- <h1>Visa Info</h1>
     <div 
       v-for="visa in visas"
       :key="visa.name">
       <router-link :to="{ name: 'visa-page', params: { slug: visa.slug } }">{{ visa.name }}</router-link>
+    </div> -->
+
+    <h1>Looking for an overview of all UK visas?</h1>
+
+    <div class="buttons">
+      <p>To start, let us know where you are from.</p>
+      <div class="container">
+        <button>Non - EU</button>
+        <button>EU</button>
+        <button>Help Centre</button>
+      </div>
     </div>
   </section>
 </template>
@@ -20,3 +31,25 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+
+.content {
+  padding: $top-padding 0;
+}
+
+.buttons {
+  margin-top: 14vw;
+
+  .container {
+    text-align: center;
+  }
+
+  button {
+    width: 125px;
+    margin: $spacing*4 $spacing 0;
+  }
+}
+
+</style>
