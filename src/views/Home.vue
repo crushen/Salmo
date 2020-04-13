039e5a1e30e6bd0a81084aea8759470d54176c31
Charlotte Rushen <charlotterushen@hotmail.com>
2020-04-13 12:31:50 +0100

    New home page background image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <section class="content">
-    <header>
-      <div class="logo">
-        <div class="circle"></div>
-        <h1 class="logo-text">Salmo</h1>
+  <section>
+    <header :style="{ backgroundImage: `url(${background})` }">
+      <div class="content">
+        <div class="logo">
+          <div class="circle"></div>
+          <h1 class="logo-text">Salmo</h1>
+        </div>
+        <h2 class="caption">The ultimate visa organisation tool.</h2>
       </div>
-      <h2 class="caption">The ultimate visa organisation tool.</h2>
     </header>
 
-    <section 
-      v-if="user"
-      class="welcome">
-      <h2 class="header">Welcome back, {{ user.profile.firstName }}.</h2>
-      <div class="placeholder-image"></div>
+    <section class="welcome">
+      <div class="content">
+        <div class="placeholder-image"></div>
+        <h2 v-if="user" class="header">Welcome back, {{ user.profile.firstName }}.</h2>
+      </div>
     </section>
 
     <news />
@@ -28,6 +30,7 @@
 </template>
 
 <script>
+import background from '@/assets/home-background.svg';
 import news from '@/components/news/News';
 import pageLinkCard from '@/components/PageLinkCard';
 
@@ -39,6 +42,7 @@ export default {
   data() {
     return {
       user: this.$store.state.auth.user,
+      background,
       pageLinks: [
         {
           text: 'Head over to your results page to see and overview of which visa suits you best.',
@@ -63,31 +67,40 @@ export default {
 
 header {
   width: 100%;
+  height: 80vw;
   margin: auto;
   text-align: center;
   padding-top: $top-padding;
+  background-size: cover;
+  background-position: 50% 0%;
 }
 
 .logo {
-  margin: 0 auto 9vw auto;
+  margin: 0 auto 4vw auto;
   display: flex;
   align-items: center;
   justify-content: center;
 
-  & .circle {
-    width: 13vw;
-    height: 13vw;
+  .circle {
+    width: 12vw;
+    height: 12vw;
     background: $light-grey;
-    margin-right: $spacing;
+    margin-right: 4px;
     border-radius: 100px;
   }
 }
 
 .welcome {
-  margin-top: 16vw;
+  transform: translateY(-20vw);
 
-  & h2 {
-    margin-bottom: 7vw;
+  .placeholder-image {
+    width: 40vw;
+    height: 55vw;
+    background: $light-grey;
+  }
+
+  h2 {
+    margin-top: 4vw;
   }
 }
 </style>
