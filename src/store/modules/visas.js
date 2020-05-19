83c01f687bd8b74c35a0f72a074b922ea2b054ad
Charlotte Rushen <charlotterushen@hotmail.com>
2020-05-19 13:12:03 +0100

    Doc checklist in store & cloud funcs- needs update

--- src/store/modules/visas.js
+++ src/store/modules/visas.js
@@ -436,7 +436,92 @@ const visas = [
 
 const documentChecklist = [
   {
-    name: 'Tier 1 Investor',
+    name: 'Tier 5 Creative and Sporting',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 5 Government Authorised Exchange',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 2 Intra-company Transfer',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Family (needs expanding)',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Global Talent',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Innovator',
     checklist: [
       { 
         label: 'Current passport (or travel documentation with blank page)',
@@ -454,6 +539,193 @@ const documentChecklist = [
   },
   {
     name: 'Startup',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 1 Investor',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 2 General Work',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 2 Minister of Religion',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 2 Sportsperson',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 4 General Student',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 4 Short Term Study',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 5 Charity Worker',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 5 International Agreement',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 5 Religious Worker',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 5 Seasonal Worker',
+    checklist: [
+      { 
+        label: 'Current passport (or travel documentation with blank page)',
+        checked: false
+      },
+      { 
+        label: 'TB test results',
+        checked: false
+      },
+      { 
+        label: "Criminal record certificate (from any country you've been in for longer than 12 months in the last 10 years)",
+        checked: false
+      }
+    ]
+  },
+  {
+    name: 'Tier 5 Youth Mobility Scheme',
     checklist: [
       { 
         label: 'Current passport (or travel documentation with blank page)',
