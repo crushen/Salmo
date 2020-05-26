060de9008b6226e7628081d17fc8a97292349a25
Charlotte Rushen <charlotterushen@hotmail.com>
2020-05-26 17:39:52 +0100

    Brexit timeline done

--- src/components/BrexitTimeline.vue
+++ src/components/BrexitTimeline.vue
@@ -1,76 +1,22 @@
 <template>
-  <div class="horizontal-scroll-container section-margin">
-    <div class="timeline">
-      <div class="top"></div>
-      <div class="middle"></div>
+  <ul class="horizontal-scroll-container section-margin">
+    <div class="line"></div>
+    <li 
+      v-for="(event, index) in events"
+      :key="event.title"
+      @click="selected = index"
+      :class="{'selected': selected === index}"
+      class="card">
+      <p class="date">{{ event.date }}</p>
+      <p class="title">{{ event.title }}</p>
 
-      <div class="bottom">
-        <div class="years">
-          <div class="medium one">
-            <p>2016</p>
-          </div>
-          <div class="small two">
-            <p>2017</p>
-            <p>2018</p>
-            <p>2019</p>
-          </div>
-          <div class="big three">
-            <p>2020</p>
-          </div>
-          <div class="medium four">
-            <p>2021</p>
-          </div>
-        </div>
-
-        <div class="months">
-          <div class="one">
-            <div class="inner">
-              <p>June</p>
-            </div>
-          </div>
-
-          <div class="two">
-            <div class="inner"></div>
-          </div>
-
-          <div class="three">
-            <div class="inner">
-              <p 
-                v-for="month in months"
-                :key="month">
-                {{ month }}
-              </p>
-            </div>
-          </div>
-
-          <div class="four">
-            <div class="inner">
-              <p>July</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="events">
-          <div 
-            v-for="(event, index) in events"
-            :key="index"
-            class="event">
-            <div class="line"></div>
-            <div
-              @click="open(index)"
-              :class="{'open': index === selectedTab}" 
-              class="tab">
-              <p v-if="!openTab || index !== selectedTab">{{ event.date }}</p>
-              <div v-else-if="openTab && index === selectedTab">
-                <p>{{ event.date }}</p>
-                <p class="text">{{ event.text }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+      <p 
+        v-if="selected === index"
+        class="text">
+        {{ event.text }}
+      </p>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -78,45 +24,69 @@
 export default {
   data() {
     return {
-      openTab: false,
-      selectedTab: null,
-      months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      selected: 0,
       events: [
         {
-          date: '23 Jun',
-          text: 'UK votes to leave the European Union.'
+          date: '23 Jun 2016',
+          title: 'UK votes to leave the EU',
+          text: 'Despite early polls suggesting otherwise, the Leave campaign won by 1.3 million votes (Leave 51.9% to Remain 48.1%).'
         },
         {
-          date: '31 Jan',
-          text: 'UK votes to leave the European Union.'
+          date: '17 Jan 2017',
+          title: 'Brexit must be delivered',
+          text: 'Theresa May comes out and says any ties with the EU will hold the UK back and claims she will deliver a "hard Brexit".'
         },
         {
-          date: '1 Mar',
-          text: 'UK votes to leave the European Union.'
+          date: '8 Jun 2017',
+          title: 'First general election',
+          text: 'In an effort to increase her majorty in parliament, May called an election. However this backfired and the Conservatives lost some power.'
         },
         {
-          date: '1 June',
-          text: 'UK votes to leave the European Union.'
+          date: '6 July 2018',
+          title: 'Chequers chat',
+          text: 'May came up with the "Chequers" deal in a bid to please both Remainers and Leavers. The ineffective deal pleased neither.'
         },
         {
-          date: '1 July',
-          text: 'UK votes to leave the European Union.'
+          date: '12 Mar 2019',
+          title: "Let's try again",
+          text: 'May tries again to get her deal through parliament, but instead suffers another huge defeat.'
         },
         {
-          date: '26 Nov',
-          text: 'UK votes to leave the European Union.'
+          date: '29 Mar 2019',
+          title: 'Settlement Scheme opens',
+          text: 'Home Office sets up a scheme to help EU people claim residency. However there have been concerns on how clear this system is.'
         },
         {
-          date: '31 Dec',
-          text: 'UK votes to leave the European Union.'
+          date: '24 Jul 2019',
+          title: 'Boris steps in',
+          text: 'Boris Johnson takes over as PM as May departs Number 10 on the 24 June.'
+        },
+        {
+          date: '12 Dec 2019',
+          title: 'Yet another election',
+          text: 'After months of deadlock, Johnson calls for an election to increase his power in parliament. He is successful and his deal brought in.Boris Johnson takes over as PM as May departs Number 10 on the 24 June.'
+        },
+        {
+          date: '31 Jan 2020',
+          title: 'Departure Starter',
+          text: 'Britain officially starts the process of leaving the EU. Protests are seen across the captial in support of the EU and calls to cancel Brexit.'
+        },
+        {
+          date: '1 Jun 2020',
+          title: 'Stocktaking',
+          text: 'A summit to assess the progress of Brexit talks is to take place in June. However COVID-19 has cast doubts whether this will be possible.'
+        },
+        {
+          date: '31 Dec 2020',
+          title: 'Free movement ends',
+          text: "Johnson's hard attitude to Brexit means that whether or not a deal is reached, free movement will end on this date."
+        },
+        {
+          date: '30 Jun 2021',
+          title: 'Last chance',
+          text: 'The government has allowed a 6th month "grace period" for individuals to apply for citizenship in the UK. After this date it will become harder to apply.'
         }
       ]
-    }
-  },
-  methods: {
-    open(index) {
-      this.selectedTab = index;
-      this.openTab = true;
     }
   }
 }
@@ -128,226 +98,73 @@ export default {
 .horizontal-scroll-container {
   overflow-x: scroll;
   overflow-y: hidden;
-  white-space: nowrap;
-  padding-top: 20px;
+  display: flex;
+  align-items: flex-start;
+  height: 250px;
+  padding: 0 7.5%;
+  position: relative;
+}
+
+.line {
+  width: 1800px;
+  height: 12px;
+  background: $secondary-light-blue;
+  position: absolute;
+  top: 20px;
+  left: 7.5%;
+  z-index: 0;
+}
+
+.card {
+  list-style: none;
+  min-height: 35%;
+  background: $secondary-blue;
+  border-radius: $border-radius;
+  min-width: 115px;
+  padding: $spacing*2 $spacing;
+  margin-right: 40px;
+  position: relative;
+  z-index: 10;
+
+  &.selected {
+    background: $primary-blue;
+    min-width: 180px;
+  }
+
+  &:nth-of-type(5),
+  &:nth-of-type(12),
+  &:nth-of-type(13) {
+    background: $secondary-pink;
+
+    &.selected {
+      background: $primary-pink;
+      min-width: 180px;
+    }
+  }
+
+  // To add margin to end of timeline
+  &:last-child:after {
+    content: "";
+    width: 40px;
+    height: 1px;
+    position: absolute;
+    left: 100%;
+    top: 0px;
+  }
 
   p {
-    font-weight: 500;
-  }
-}
+    color: white;
+    font-size: 14px;
+    font-weight: 400;
 
-.timeline {
-  height: 180px;
-  width: 1580px;
-  background: white;
-  .top,
-  .middle {
-    height: 38%;
-    border-top: 3px solid $light-grey;
-  }
-
-  .bottom {
-    height: 24%;
-  }
-}
-
-.years {
-  display: flex;
-  color: $light-grey;
-  margin-left: 70px;
-
-  p {
-    font-weight: 900;
-  }
-
-  .one {
-    margin-right: 50px;
-    color: darken($light-grey, 13%);
-  }
-
-  .two {
-    margin-right: 40px;
-    color: lighten($light-grey, 3%);
-  }
-
-  .three {
-    margin-right: 964px;
-    color: darken($light-grey, 5%);
-  }
-
-  .four {
-    color: darken($light-grey, 13%);
-  }
-
-  .small {
-    display: flex;
-
-    p {
-      font-size: 16px;
-      transform: translateY(-18px);
-      margin-right: 4px;      
-    }
-  }
-
-  .medium {
-    display: flex;
-
-    p {
-      font-size: 35px;
-      transform: translateY(-39px);
-    }
-  }
-
-  .big {
-    display: flex;
-
-    p {
-      font-size: 60px;
-      transform: translateY(-67px);
-    }
-  }
-}
-
-.months {
-  display: flex;
-  height: 100%;
-  transform: translateY(-86px);
-
-  .one {
-    background: lighten($light-grey, 3%);
-    width: 170px;
-
-    .inner {
-      height: 100%;
-      padding-top: 8px;
-      background: darken($light-grey, 13%);
-      border-bottom-right-radius: 25px;
+    &.date {
+      font-weight: 700;
+      margin-bottom: $spacing;
     }
 
-    p {
-      margin-left: 72px;
+    &.text {
+      margin-top: $spacing;
     }
-  }
-
-  .two {
-    background: darken($light-grey, 5%);
-    width: 150px;
-
-    .inner {
-      height: 100%;
-      padding-top: 8px;
-      background: lighten($light-grey, 3%);
-      border-bottom-right-radius: 25px;
-    }
-    
-  }
-
-  .three {
-    background: darken($light-grey, 13%);
-    width: 1060px;
-
-    .inner {
-      height: 100%;
-      padding: 8px 0 0 32px;
-      display: flex;
-      background: darken($light-grey, 5%);
-      border-bottom-right-radius: 25px;
-    }
-
-    p:not(:first-of-type) {
-      margin-left: 32px;
-    } 
-    
-  }
-
-
-  .four {
-    background: darken($light-grey, 13%);
-    width: 200px;
-
-    .inner {
-      height: 100%;
-      padding: 8px 0 0 72px;
-      background: darken($light-grey, 13%);
-      border-bottom-right-radius: 25px;
-    }
-  }
-}
-
-.events {
-  transform: translateY(-210px);
-  display: flex;
-
-  .event {
-    position: absolute;
-    width: 60px;
-
-    &:nth-of-type(even) {
-      .tab {
-        &.open {
-          bottom: -30px;
-        }
-      }
-    }
-  }
-
-  .tab {
-    background: $secondary-blue;
-    color: $light-font;
-    border-radius: 4px;
-    padding: 3px 6px;
-    white-space: normal;
-    transition: 0.2s;
-    position: absolute;
-    z-index: 5;
-
-    &.open {
-      width: 150px;
-      left: -75%;
-    }
-
-    .text {
-      font-weight: 300;
-    }
-  }
-
-  .line {
-    width: 3px;
-    height: 81px;
-    background: $secondary-blue;
-    position: absolute;
-    z-index: 0;
-    top: 0;
-    left: calc(50% - 3px);
-  }
-
-  .event:nth-of-type(odd) {
-    top: -69px;
-    margin-left: 58px;
-
-    .line {
-      height: 150px;
-    }
-  }
-
-  .event:nth-of-type(2) {
-    margin-left: 350px;
-  }
-
-  .event:nth-of-type(3) {
-    margin-left: 530px;
-  }
-
-  .event:nth-of-type(4) {
-    margin-left: 720px;
-  }
-
-  .event:nth-of-type(5) {
-    margin-left: 810px;
-  }
-
-  .event:nth-of-type(6) {
-    margin-left: 1170px;
   }
 }
 </style>
