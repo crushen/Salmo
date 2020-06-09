e2ec8e6c75647a9e425ba2f4801d9729ca0b5946
Charlotte Rushen <charlotterushen@hotmail.com>
2020-06-09 11:12:57 +0100

    Terms and conditions

--- src/components/profile/TermsAndConditions.vue
+++ src/components/profile/TermsAndConditions.vue
@@ -1,11 +1,126 @@
 <template>
-  <section class="scroll-container">
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error placeat mollitia nemo, quaerat, dicta repudiandae quisquam odit aut alias nesciunt assumenda ipsam voluptatibus cupiditate dolorum, necessitatibus cumque suscipit impedit natus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error placeat mollitia nemo, quaerat, dicta repudiandae quisquam odit aut alias nesciunt assumenda ipsam voluptatibus cupiditate dolorum, necessitatibus cumque suscipit impedit natus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error placeat mollitia nemo, quaerat, dicta repudiandae quisquam odit aut alias nesciunt assumenda ipsam voluptatibus cupiditate dolorum, necessitatibus cumque suscipit impedit natus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error placeat mollitia nemo, quaerat, dicta repudiandae quisquam odit aut alias nesciunt assumenda ipsam voluptatibus cupiditate dolorum, necessitatibus cumque suscipit impedit natus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error placeat mollitia nemo, quaerat, dicta repudiandae quisquam odit aut alias nesciunt assumenda ipsam voluptatibus cupiditate dolorum, necessitatibus cumque suscipit impedit natus.</p>
-  </section>
+  <article class="scroll-container">
+    <section>
+      <h1>These terms and conditions using your Salmo account</h1>
+      <p>These legal terms (English law applies and disputes will be settled by English courts) are between you and Salmo Visa Limited (Salmo/us/we) and you agree to them by using the Salmo web app. Your Salmo account is a personal identity account and you mustn’t use it for business purposes.</p>
+    </section>
+
+    <section>
+      <h1>You should read this document along with our Data Privacy Notice</h1>
+    </section>
+
+    <section>
+      <h1>How to contact us:</h1>
+      <p>Email us at help@salmo.uk.</p>
+    </section>
+
+    <section>
+      <h1>Or send a letter to:</h1>
+      <p>Salmo Visa Limited, Studio 4, Replica House, 37 Bavaria Road, London N19 4EU, United Kingdom.</p>
+    </section>
+
+    <section>
+      <h1>How we'll contact you</h1>
+      <p>We'll contact you in clear and simple English via email or home address. Please let us know if any of these change! We also send you instant notifications to remind your visa expiration and application deadline.</p>
+    </section>
+
+    <section>
+      <h1>We may block your payments if:</h1>
+      <ul>
+        <li>your instructions are unclear</li>
+        <li>we suspect criminal activity on your account</li>
+      </ul>
+
+      <p>If we block a payment, we'll let you know as soon as possible, using one of our usual channels (see ‘How we’ll contact you’ above).</p>
+
+      <p class="margin">You can find information on all your transactions in your feed and your regular payment methods in the app. There’s information on how to make different payments, the information we need about the recipient and how long these payments will take, too.</p>
+    </section>
+
+    <section>
+      <h1>Make a payment from abraod</h1>
+      <p>When you use your card abroad to make a payment in a foreign currency, we use Mastercard’s exchange rate with a 0.0% fee.</p>
+    </section>
+
+    <section>
+      <h1>16-17 year olds</h1>
+      <p>If you’re under 18 you won’t be able to apply for any paid plan without the authorisation with your parent (s), and we won’t charge any fees if you go into an unarranged overdraft. From the age of 18, you will be able to make payments independently.</p>
+    </section>
+
+    <section>
+      <h1>You agree to us using your information</h1>
+      <p>By accepting these terms, you agree to us using your information to make and collect personal data from your account. If you’re no longer happy for us to use your information, we’ll have to close your account. But we may keep your personal data and use it where we have lawful grounds to do so. For example, any records we need to keep for regulatory reasons (see our Data Privacy Notice).</p>
+    </section>
+
+    <section>
+      <h1>Fees and Charges</h1>
+      <p>Here are our fees for using the main services on your current account. You may have to pay other costs, taxes or charges in relation to your Salmo account, which are outside of our control and not charged by us. For example, banks may charge you for sending money from your bank account.</p>
+    </section>
+
+    <section>
+      <h1>When we offer rewards</h1>
+      <p>Sometimes we’ll offer rewards for things like inviting friends to join Salmo. We can withdraw or change these offers any time, without letting you know in advance. You’ll only get financial awards once per action (for example you’ll only get a bonus for once per friend you invite). You might have to pay income tax on money you get as part of these rewards. Contact HMRC if you’re unsure.</p>
+    </section>
+
+    <section>
+      <h1>If something goes wrong</h1>
+      <p>Please keep your account username and password safe at all times. If we discover any security issues affecting your account, we’ll contact you as soon as possible in the most secure way, using one of our usual channels (see ‘How we’ll contact you’ above).</p>
+
+      <p class="margin">If you forgot your username or password, please refer to “Forgot Password”.</p>
+    </section>
+
+    <section>
+      <h1>We’ll usually refund you any money if:</h1>
+      <ul>
+        <li>we should’ve applied strong customer authentication but we didn’t, unless you acted fraudulently</li>
+        <li>someone makes a payment without your permission</li>
+      </ul>
+
+      <p>We’ll also refund any money you lost due to our mistakes or inaccuracies with your payments. We can help by speaking to other banks to make sure they treat any payment we send late as if it was sent it on time.</p>
+    </section>
+
+    <section>
+      <h1>But you won’t be able to claim back money you’ve lost if:</h1>
+      <ul>
+        <li>you gave us incorrect instructions or we can prove that the bank we sent your payment to received it (although we’ll still try to help you recover your money)</li>
+        <li>you purposefully didn’t keep your account details safe, or you were very negligent in not keeping them safe or if your account is in overdraft, you gave them to someone else</li>
+        <li>you purposefully didn’t keep your account details safe, or you were very negligent in not keeping them safe or if your account is in overdraft, you gave them to someone else</li>
+        <li>you acted fraudulently</li>
+      </ul>
+
+      <p>Errors with Direct Debits are covered by the Direct Debit guarantee scheme.</p>
+    </section>
+
+    <section>
+      <h1>How to make a complaint</h1>
+      <p>If you have a complaint, please contact us and we’ll do our best to fix the problem.</p>
+
+      <p class="margin">If you’re still not happy, you can refer your complaint to the Office of the Immigration Services Commissioner. For more details you can visit their website at https://www.gov.uk/government/organisations/office-of-the-immigration-services-commissioner</p>
+    </section>
+
+    <section>
+      <h1>Making changes to this agreement</h1>
+      <p>This agreement will always be available in the app and on our website.</p>
+
+      <p class="margin">We can make changes to our charges or the terms of this agreement from time to time. This includes introducing new charges or rates and charging in a different way. We may make these changes, including fees we charge, because of changes to:</p>
+
+      <ul class="margin">
+        <li>law, regulation, industry codes, court or regulator decisions</li>
+        <li>the way we run our business</li>
+        <li>the cost of providing your account and running our business, for example a change in our cost of funding, technology (including our systems) and service costs</li>
+        <li>the visa application system</li>
+      </ul>
+
+      <p class="margin">We can make favourable changes to these terms and charges for any reason. We can also make proportionate changes for any other balanced and valid reason that impacts Salmo or your account.</p>
+
+      <p class="margin">If we make changes to it that are clearly in your favour, we’ll tell you once we’ve made them. Otherwise we’ll give you two months’ notice and tell you our reasons in the most secure way, using one of our usual channels (see ‘How we’ll contact you’ above).</p>
+
+      <p class="margin">If you don’t agree to these changes, you can let us know and we’ll close your account with no fee. We’ll delete any personal information and record in 1 working day. If we don’t hear from you before the changes come into effect, we’ll assume that you’re happy and accept the changes we’ve made.</p>
+    </section>
+
+    <section>
+      <p class="address">Salmo Visa Limited. Registered in England. Registered No. 12482305. Registered Office: Studio 4, Replica House, 37 Bavaria Road, London, N19 4EU.</p>
+    </section>
+  </article>
 </template>
 
 <style lang="scss" scoped>
@@ -20,5 +135,32 @@
   width: 100%;
   height: 40vh; 
   margin: $spacing*4 0;
+}
+
+section:not(:last-of-type) {
+  margin-bottom: $spacing*3;
+}
+
+h1 {
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: $spacing;
+}
+
+p, li {
+  font-size: 14px;
+}
+
+ul {
+  margin-bottom: $spacing;
+  list-style-position: inside;
+}
+
+.margin {
+  margin-top: $spacing;
+}
+
+.address {
+  font-weight: 200;
 }
 </style>
