0caf38c45f1895b6f372aef391d9b36ac21da821
Charlotte Rushen <charlotterushen@hotmail.com>
2020-06-16 12:33:02 +0100

    Logged out home page tablet

--- src/components/home/ContentCard.vue
+++ src/components/home/ContentCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card">
-    <h3>{{ title }}</h3>
+    <h3 v-html="title"></h3>
     <div class="inner">
       <img :src="img" alt="">
       <p>{{ subTitle }}</p>
@@ -46,6 +46,50 @@ export default {
 
       img {
         margin: 0 0 0 4vw;
+      }
+    }
+  }
+}
+
+// Tablet
+@media screen and (min-width: 420px) {
+  .card {
+    margin-bottom: $spacing*12;
+
+    h3 {
+      margin: 0 0 $spacing*2 35%;
+      width: 65%;
+    }
+
+    .inner {
+      p {
+        width: 65%;
+        margin: 0 0 0 35%;
+      }
+
+      img {
+        width: 18%;
+        transform: translateY(-30px);
+        position: absolute;
+        margin: 0;
+      }
+    }
+
+    &:nth-of-type(2) {
+      h3 {
+        margin: 0 35% $spacing*2 0;
+      }
+
+      .inner {
+        flex-direction: row-reverse;
+
+        p {
+          margin: 0 35% 0 0;
+        }
+
+        img {
+          margin: 0;
+        }
       }
     }
   }
--- src/components/home/LoggedOut.vue
+++ src/components/home/LoggedOut.vue
@@ -31,17 +31,17 @@ export default {
     return {
       cards: [
         {
-          title: 'Not sure what your next step is? Overwhelmed by choices?',
+          title: 'Not sure what your next step is?<br>Overwhelmed by choices?',
           subTitle: 'Your personalised quiz can help. Answer a series of quick questions and we can suggest which visas will suit you best.',
           img: imgOne
         },
         {
-          title: 'Struggling to keep organised? Don’t know where to start?',
+          title: 'Struggling to keep organised?<br>Don’t know where to start?',
           subTitle: 'Your visa planner can help. Track and keep note of past visas, your route to Permenant Residency and any documentation you need to collect.',
           img: imgTwo
         },
         {
-          title: "Find info elsewhere confusing? Not sure what's relevant to you?",
+          title: "Find info elsewhere confusing?<br>Not sure what's relevant to you?",
           subTitle: "All results are personalised for you, so you see the info you need. Visas are broken down into more consise and managable sections, giving you back the power.",
           img: imgThree
         }
