f0239fdabc5ced4935b4baac89392ffd5403d898
Charlotte Rushen <charlotterushen@hotmail.com>
2020-07-01 16:47:33 +0100

    Help center page data now in store

--- src/views/HelpCentrePage.vue
+++ src/views/HelpCentrePage.vue
@@ -5,8 +5,10 @@
 
       <div class="body">
         <h3>{{ currentPage.subTitle }}</h3>
-
-        <img :src="currentPage.img.url" :alt="currentPage.img.alt">
+        
+        <img 
+          :src="currentPage.img.url" 
+          :alt="currentPage.img.alt">
 
         <div class="text">
           <p 
@@ -36,11 +38,9 @@
 </template>
 
 <script>
-import brpImg from '@/assets/illustrations/about/BRP-guide.svg';
-import ilrImg from '@/assets/illustrations/about/ILR-Guide.svg';
-import switchImg from '@/assets/illustrations/about/Switch-Extend.svg';
-import dependantsImg from '@/assets/illustrations/about/Applying with Dependants.svg';
 import helpCentreFeedback from '@/components/HelpCentreFeedback';
+
+import { mapState } from 'vuex';
 
 export default {
   components: {
@@ -48,70 +48,13 @@ export default {
   },
   data() {
     return {
-      slug: this.$route.params.slug,
-      pages: [
-        {
-          title: 'Biometric Residence Permit',
-          slug: 'biometric-residence-permit',
-          subTitle: 'Sometimes reffered to as a BRP, this process is necessary for anyone on a visa in the UK.',
-          text: [
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?',
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?',
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?'
-          ],
-          img: {
-            url: brpImg,
-            alt: 'An illustration of a person holding their Biometric Residence Permit'
-          }
-        },
-        {
-          title: 'Applying with Dependants',
-          slug: 'applying-with-dependants',
-          subTitle: 'Sometimes reffered to as a BRP, this process is necessary for anyone on a visa in the UK.',
-          text: [
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?',
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?',
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?'
-          ],
-          img: {
-            url: dependantsImg,
-            alt: 'An illustration of a father and daughter holding hands'
-          }
-        },
-        {
-          title: 'Switch VS Extend',
-          slug: 'switch-vs-extend',
-          subTitle: 'Sometimes reffered to as a BRP, this process is necessary for anyone on a visa in the UK.',
-          text: [
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?',
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?',
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?'
-          ],
-          img: {
-            url: switchImg,
-            alt: 'An illustration of a person thinking'
-          }
-        },
-        {
-          title: 'Indefinite Leave to Remain',
-          slug: 'indefinite-leave-to-remain',
-          subTitle: 'Sometimes reffered to as a BRP, this process is necessary for anyone on a visa in the UK.',
-          text: [
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?',
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?',
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perferendis quo deserunt neque optio, quaerat, ipsam quia enim voluptatem magni illum facere atque explicabo voluptas porro ducimus eos aperiam nulla?'
-          ],
-          img: {
-            url: ilrImg,
-            alt: 'An illustration of a person holding their passport with excitement'
-          }
-        }
-      ]
+      slug: this.$route.params.slug
     }
   },
   computed: {
+    ...mapState('helpCentre', ['helpPages']),
     currentPage() {
-      return this.pages.find(page => page.slug === this.slug);
+      return this.helpPages.find(page => page.slug === this.slug);
     }
   },
   mounted() {
