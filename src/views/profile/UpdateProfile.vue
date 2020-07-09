ba40875fd10fed2fb54d07200ac4a1e85a380e06
Charlotte Rushen <charlotterushen@hotmail.com>
2020-07-09 12:48:34 +0100

    New delete visa option and new layout

--- src/views/profile/UpdateProfile.vue
+++ src/views/profile/UpdateProfile.vue
@@ -10,6 +10,15 @@
           @cancel="showAlert = false, $router.push(to)"
           @confirm="showAlert = false, to = null, $router.go(1)" />
       </transition>
+      <transition name="alert" mode="out-in">
+        <alert 
+          v-if="deleteVisa"
+          alert="Are you sure you want to delete this visa?"
+          text="You can always add a visa later on."
+          :buttons="['No Thanks', 'Yes, Delete This Visa']"
+          @cancel="deleteVisa = false"
+          @confirm="confirmRemove" />
+      </transition>
 
       <h1>Edit profile.</h1>
 
@@ -79,84 +88,145 @@
           <p v-if="errors.nationality" class="error">Nationality is required</p>
         </div>
 
-        <div class="row">
-          <div class="field current-visa">
-            <label for="current-visa">Current visa</label>
-            <select
-              class="form" 
-              v-model="profileToUpdate.currentVisa.name"
-              name="Current Visa"
-              id="current-visa">
-              <option
-                v-for="visa in visas"
-                :key="visa"
-                :value="visa">
-                {{ visa }}
-              </option>
-            </select>
-          </div>
-
-          <div class="field visa-dates">
-            <div>
-              <label for="current-start" class="form-label">Start date</label>
-              <input 
-                v-model="profileToUpdate.currentVisa.start"
-                type="date"
-                id="current-start"
-                class="form unstyled">
-            </div>
-
-            <div>
-              <label for="current-end" class="form-label">End date</label>
-              <input 
-                v-model="profileToUpdate.currentVisa.end"
-                type="date"
-                id="current-end"
-                class="form unstyled">
-            </div>
+        <h3 class="current-visa-title">Current Visa</h3>
+        <div 
+          v-if="!editCurrentVisa"
+          class="current-visa-text">
+          <div class="row">
+            <p class="visa">{{ user.currentVisa.name }}</p>
+            <p>{{ date(user.currentVisa.start) }} - {{ date(user.currentVisa.end) }}</p>
+            <button 
+              @click="editCurrentVisa = true"
+              class="tertiary">
+              Edit Visa
+            </button>
+            <div class="space"></div>
           </div>
         </div>
-        <p v-if="errors.currentVisa.required" class="error">Current visa name and dates are required</p>
-        <p v-if="errors.currentVisa.dates" class="error">Current visa start date can't be after end date</p>
 
+        <div v-else>
+          <div class="row">
+            <div class="field current-visa">
+              <label for="current-visa">Current visa</label>
+              <select
+                class="form" 
+                v-model="profileToUpdate.currentVisa.name"
+                name="Current Visa"
+                id="current-visa">
+                <option
+                  v-for="visa in visas"
+                  :key="visa"
+                  :value="visa">
+                  {{ visa }}
+                </option>
+              </select>
+            </div>
+
+            <div class="field visa-dates">
+              <div>
+                <label for="current-start" class="form-label">Start date</label>
+                <input 
+                  v-model="profileToUpdate.currentVisa.start"
+                  type="date"
+                  id="current-start"
+                  class="form unstyled">
+              </div>
+
+              <div>
+                <label for="current-end" class="form-label">End date</label>
+                <input 
+                  v-model="profileToUpdate.currentVisa.end"
+                  type="date"
+                  id="current-end"
+                  class="form unstyled">
+              </div>
+            </div>
+          </div>
+          <p v-if="errors.currentVisa.required" class="error">Current visa name and dates are required</p>
+          <p v-if="errors.currentVisa.dates" class="error">Current visa start date can't be after end date</p>
+        </div>
+
+        <h3 
+          v-if="profileToUpdate.pastVisas.length"
+          class="past-visas-title">
+          Past Visas
+        </h3>
         <div
           v-for="(visa, index) in profileToUpdate.pastVisas"
-          :key="index"
-          class="row">
-          <div class="field past-visa">
-            <label :for="`past-visa-${index}`">Past visa</label>
-            <select
-              class="form" 
-              v-model="visa.name"
-              name="Current Visa"
-              :id="`past-visa-${index}`">
-              <option
-                v-for="item in pastVisas"
-                :key="item"
-                :value="item">
-                {{ item }}
-              </option>
-            </select>
+          :key="index">
+          <div v-if="editPastVisa === index">
+            <div class="row">
+              <div class="field past-visa">
+                <label :for="`past-visa-${index}`">Past visa</label>
+                <select
+                  class="form" 
+                  v-model="visa.name"
+                  name="Current Visa"
+                  :id="`past-visa-${index}`">
+                  <option
+                    v-for="item in pastVisas"
+                    :key="item"
+                    :value="item">
+                    {{ item }}
+                  </option>
+                </select>
+              </div>
+
+              <div class="field visa-dates">
+                <div>
+                  <label :for="`past-start-${index}`" class="form-label">Start date</label>
+                  <input 
+                    v-model="visa.start"
+                    type="date"
+                    :id="`past-start-${index}`"
+                    class="form unstyled">
+                </div>
+
+                <div>
+                  <label :for="`current-end-${index}`" class="form-label">End date</label>
+                  <input
+                    v-model="visa.end"
+                    type="date"
+                    :id="`current-end-${index}`"
+                    class="form unstyled">
+                </div>
+              </div>
+            </div>
+
+            <button
+              @click="removePastVisa(index)"
+              class="tertiary remove"
+              type="button">
+              Remove this visa
+            </button>
           </div>
 
-          <div class="field visa-dates">
-            <div>
-              <label :for="`past-start-${index}`" class="form-label">Start date</label>
-              <input 
-                v-model="visa.start"
-                type="date"
-                :id="`past-start-${index}`"
-                class="form unstyled">
-            </div>
 
-            <div>
-              <label :for="`current-end-${index}`" class="form-label">End date</label>
-              <input
-                v-model="visa.end"
-                type="date"
-                :id="`current-end-${index}`"
-                class="form unstyled">
+          <div 
+            v-else 
+            :class="{ first: index === 0 }"
+            class="past-visa-text">
+            <div class="row">
+              <p class="visa">{{ visa.name }}</p>
+              <p>{{ date(visa.start) }} - {{ date(visa.end) }}</p>
+              <button 
+                @click="editPastVisa = index"
+                class="tertiary">
+                Edit Visa
+              </button>
+
+              <div 
+                @click="removePastVisa(index)"
+                class="tertiary delete-button">
+                Delete
+              </div>
             </div>
+            
+            <!-- <div 
+              @click="removePastVisa(index)"
+              class="delete">
+              <img src="@/assets/icons/white/times-solid.svg" alt="">
+            </div> -->
           </div>
         </div>
         <p v-if="errors.pastVisas.required" class="error">Past visa name and dates are required</p>
@@ -194,11 +264,15 @@ export default {
     return {
       waveV,
       showAlert: false,
+      deleteVisa: false,
+      removeVisa: null,
       to: null,
       user: this.$store.state.auth.user.profile,
       finished: false,
       newVisaAdded: false,
       currentPastVisas: null,
+      editCurrentVisa: false,
+      editPastVisa: null,
       errors: {
         birthday: false,
         nationality: false,
@@ -601,6 +675,8 @@ export default {
     addPastVisa() {
       this.newVisaAdded = true;
       this.profileToUpdate.pastVisas.push({name: null, start: null, end: null});
+      const newVisaIndex = this.profileToUpdate.pastVisas.length - 1;
+      this.editPastVisa = newVisaIndex;
     },
     calculateAge(date) {
       let today = new Date(),
@@ -610,6 +686,23 @@ export default {
       if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) { age--; }
       this.age = age;
       return age;
+    },
+    removePastVisa(index) {
+      this.removeVisa = index;
+      this.deleteVisa = true;
+      const overlay = document.querySelector('#overlay');
+      overlay.style.opacity = 1;
+      overlay.style.visibility = 'visible';
+      document.querySelector('body').style.overflow = 'hidden';
+    },
+    confirmRemove() {
+      this.deleteVisa = false;
+      this.profileToUpdate.pastVisas.splice(this.removeVisa, 1);
+      this.$store.dispatch('auth/updateProfile', this.profileToUpdate);
+    },
+    date(oldDate) {
+      const newDate = oldDate.split('-');
+      return newDate.reverse().join('/');
     }
   },
   watch: {
@@ -659,7 +752,8 @@ export default {
 }
 
 .container {
-  margin-top: $spacing*5;
+  margin: $spacing*5 auto 0 auto;
+  max-width: 600px;
 }
 
 h3 {
@@ -672,7 +766,7 @@ h3 {
   }
 
   &.current-visa {
-    padding-top: $spacing*7;
+    padding-top: $spacing*3;
   }
 }
 
@@ -682,7 +776,77 @@ h3 {
 }
 
 .past-visa {
-  margin-top: $spacing*7;
+  margin-top: $spacing*3;
+}
+
+.current-visa-title {
+  font-weight: 400;
+  margin-top: $spacing*5;
+}
+
+.current-visa-text {
+  margin-top: $spacing*3;
+
+  .visa {
+    color: $primary-pink;
+    font-weight: 500;
+    width: 130px;
+  }
+
+  .tertiary {
+    margin: $spacing 0 0;
+    font-size: 16px;
+  }
+
+  .space {
+    display: none;
+  }
+}
+
+.past-visas-title {
+  font-weight: 400;
+  margin: $spacing*5 0 $spacing*2;
+}
+
+.past-visa-text {
+  margin-top: $spacing*3;
+  position: relative;
+  width: 90%;
+
+  &.first {
+    margin-top: 0;
+  }
+
+  .visa {
+    font-weight: 500;
+    width: 130px;
+  }
+
+  .tertiary {
+    margin: $spacing 0 0;
+    font-size: 16px;
+  }
+
+  .delete-button {
+    margin-left: $spacing*3;
+    display: inline-block;
+  }
+  // .delete {
+  //   width: 30px;
+  //   height: 30px;
+  //   border-radius: 100px;
+  //   display: flex;
+  //   align-items: center;
+  //   justify-content: center;
+  //   position: absolute;
+  //   right: 0;
+  //   top: 10px;
+  //   background: $light-grey;
+
+  //   img {
+  //     width: 14px;
+  //   }
+  // }
 }
 
 .save-changes {
@@ -696,7 +860,13 @@ h3 {
 
 .tertiary {
   font-size: 18px;
-  margin-top: $spacing*3;
+  margin-top: $spacing*5;
+
+  &.remove {
+    margin-top: $spacing;
+    color: $primary-pink;
+    font-size: 16px;
+  }
 }
 
 // Tablet
@@ -714,12 +884,14 @@ h3 {
   }
 
   .field {
+    margin-top: $spacing*3;
+
     &:not(:first-of-type) {
       padding-top: $spacing*2;
     }
 
     &.current-visa {
-      padding-top: $spacing*7;
+      padding-top: 0;
       margin-right: $spacing*2;
       width: 49%;
     }
@@ -730,7 +902,7 @@ h3 {
     }
 
     &.visa-dates {
-      padding-top: $spacing*7;
+      padding-top: 0;
       width: 51%;
 
       div {
@@ -741,6 +913,55 @@ h3 {
         margin-right: $spacing;
       }
     }
+  }
+
+  .current-visa-title {
+    margin-bottom: 0;
+  }
+
+  .current-visa-text {
+    .row {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .tertiary {
+      margin: 0;
+    }
+
+    .space {
+      display: block;
+      width: 47.73px;
+    }
+  }
+
+  .past-visas-title {
+    margin: $spacing*5 0 0;
+  }
+
+  .past-visa-text {
+    width: 100%;
+    margin-top: $spacing*4;
+
+    &.first {
+      margin-top: $spacing*3;
+    }
+
+    .row {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .tertiary {
+      margin: 0;
+    }
+
+    // .delete {
+    //   top: -4px;
+    //   right: -12%;
+    // }
   }
 }
 
