25bc3e8d7d1a00146c3422d99d4178fff6251760
Charlotte Rushen <charlotterushen@hotmail.com>
2020-10-06 11:51:24 +0100

    Visa options and countries imported from assets

---
+++ src/assets/js/visaOptions.js
@@ -0,0 +1,9 @@
+export const visaOptions = [
+  'Tier 1 : Global Talent',
+  'Tier 1 : Innovator',
+  'Tier 1 : Investor',
+  'Tier 1 : Startup',
+  'Tier 2 : General Work Visa',
+  'Tier 4 : General Student',
+  'Tier 5 : Youth Mobility'  
+]
