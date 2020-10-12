736b813ecd142ca14c8c8ed4f36d2f95591690d0
Charlotte Rushen <charlotterushen@hotmail.com>
2020-10-12 14:32:30 +0100

    Home - mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <section id="home" class="content">
-    <router-link :to="{ name: 'sign-up' }" tag="button">
-      Sign Up
-    </router-link>
+  <main>
+    <div
+      @click="morph"
+      class="wave"
+      :style="{ 
+        backgroundImage: `url(${require('@/assets/backgrounds/wave.svg')})`,
+        backgroundPosition: `${wavePosition.x}px ${wavePosition.y}px`
+      }" />
 
-    <router-link :to="{ name: 'sign-in' }" tag="button">
-      Sign In
-    </router-link>
+    <div
+      class="background"
+      :style="{ backgroundImage: `url(${require('@/assets/backgrounds/home-background.svg')})` }" />
 
-    <p>by signing up on Salmo, you agree to our Terms & Conditions and Privacy Policies.</p>
-  </section>
+    <section id="home" class="content padding bottom">
+      <div class="buttons">
+        <router-link
+          :to="{ name: 'sign-up' }"
+          tag="button"
+          class="primary wave-background">
+          Sign Up
+        </router-link>
+        <!-- :style="{ backgroundImage: `url(${require('@/assets/patterns/wave-horizontal.svg')})` }" -->
+
+        <router-link
+          :to="{ name: 'sign-in' }"
+          tag="button"
+          
+          class="secondary wave-background">
+          Sign In
+        </router-link>
+        <!-- :style="{ backgroundImage: `url(${require('@/assets/patterns/wave-verticle.svg')})` }" -->
+      </div>
+
+      <p>by signing up on Salmo, you agree to our Terms & Conditions and Privacy Policies.</p>
+    </section>
+  </main>
 </template>
 
 <script>
+
+export default {
+  data() {
+    return {
+      wavePosition: {
+        x: -50,
+        y: -800
+      }
+    }
+  },
+  methods: {
+    morph() {
+      this.wavePosition.x += 1000
+      this.wavePosition.y += 300
+    }
+  }
+}
 
 // export default {
 //   data() {
@@ -63,3 +105,70 @@
 //   }
 // }
 </script>
+
+<style lang="scss" scoped>
+
+.background {
+  width: 100%;
+  height: calc(100vh - 120px);
+  margin-top: 120px;
+  position: absolute;
+  opacity: 0.2;
+  background-position: right top;
+  z-index: 0;
+}
+
+#home {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  position: relative;
+  z-index: 5;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  button {
+    width: 152px;
+
+    &:last-of-type {
+      margin: 8px 0 32px 0;
+    }
+  }
+}
+
+p {
+  text-align: center;
+}
+
+
+
+
+
+
+.wave {
+  height: 100vh;
+  position: absolute;
+  width: 100%;
+  z-index: 12;
+  left: 0;
+  top: 0;
+  background-repeat: repeat-x;
+  //pointer-events: none;
+  transition: 2s cubic-bezier(.26,.3,.36,.94);
+  //animation: wave-animation 2.5s infinite linear;
+  z-index: 0;
+}
+
+
+
+// @keyframes wave-animation {
+//   0%   { background-position: 0 -800px; }
+//   100% {background-position: 998px -800px;}
+// }
+
+</style>
