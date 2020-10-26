0fe6cb214653e7201b21725196da40aa55ae02bc
Charlotte Rushen <charlotterushen@hotmail.com>
2020-10-26 15:45:02 +0000

    About page - mobile

---
+++ src/components/cards/AboutCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <img :src="img" alt="">
+
+    <h3 class="margin-s top bottom">{{ number }}. {{ title }}</h3>
+
+    <p v-html="text"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    number: { required: true, type: Number },
+    title: { required: true, type: String },
+    text: { required: true, type: String },
+    img: { required: true, type: String }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+
+.card {
+  width: 85%;
+  margin: 3em auto 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 60%;
+    margin: auto;
+  }
+
+  h3 {
+    font-weight: 600;
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,9 @@
-<template>
-  <section id="about">
-    <desktop-header v-if="innerWidth > 1100" />
+ <template>
+  <main id="about" class="page padding top bottom">
 
-    <div class="content">
-      <div class="logo-about">
-        <img
-          src="@/assets/logo/colour.svg" 
-          alt="Salmo logo">
-        <h1>Salmo</h1>
-      </div>
-      <h2>Out future goals for the UK visa industry.</h2>
+    <section class="content">
+      <h1>About Salmo</h1>
+      <h2 class="margin-s top">Our future goals for the UK visa industry.</h2>
 
       <about-card 
         v-for="(card, index) in cards"
@@ -18,56 +12,38 @@
         :title="card.title"
         :text="card.text"
         :img="card.img"/>
-    </div>
+    </section>
 
-    <div class="help">
-      <div class="content">
-        <p>If you’ve got a specific question about a visa related topic, our Help Centre has lots of helpful guides to all things visa!</p>
-        <div class="button">
-          <router-link 
-            :to="{name: 'help-centre'}"
-            tag="button"
-            :style="{backgroundImage: `url(${dots})`, backgroundSize: '100%', backgroundPosition: 'center'}">
-            Help Centre
-          </router-link>
-        </div>
-      </div>
-    </div>
-  </section>
+    <section class="content margin-l top">
+      <help-centre-card />
+    </section>
+  </main>
 </template>
 
 <script>
-import aboutCard from '@/components/AboutCard';
-import imgOne from '@/assets/illustrations/about/Salmo Goals 1.svg';
-import imgTwo from '@/assets/illustrations/about/Salmo goals 2.svg';
-import imgThree from '@/assets/illustrations/about/Salmo goals 3.svg';
-import dots from '@/assets/patterns/dots.svg';
-import desktopHeader from '@/components/DesktopHeader';
+import aboutCard from '@/components/cards/AboutCard'
+import helpCentreCard from '@/components/cards/HelpCentre'
 
 export default {
-  components: {
-    aboutCard,
-    desktopHeader
-  },
+  components: { aboutCard, helpCentreCard },
   data() {
     return {
       innerWidth: null,
-      dots,
       cards: [
         {
           title: 'Offer a better service for immigrants to apply for a UK visa.',
           text: 'Applying a visa is like setting your foot into a ruthless maze, with a confusing map in your hand. You may get stuck, lose resources, feel helplessly stressed and much more.<br><br>While we don’t have the power to change the entire visa system (because we’re not the Home Office), we can turn this labyrinth into a highway, so you can can retrieve the control to your path.',
-          img: imgOne
+          img: require('@/assets/illustrations/about/Salmo Goals 1.svg')
         },
         {
           title: 'Core changes at Home Office with the way that visa’s are processed.',
           text: 'Although we’re not the Home Office, we do have a grand ambition to become the first bottom-up consultancy for them, where we could represent the voice of UK immigrants and help to reshape a better visa system.',
-          img: imgTwo
+          img: require('@/assets/illustrations/about/Salmo goals 2.svg')
         },
         {
           title: "Change the way the British public sees immigrants.",
           text: 'In order to build a harmonic society, we must learn to understand each other’s viewpoint. We aim to create a community that encourages locals and immigrants to share their thoughts on each other, and to defeat immigrants myths together.',
-          img: imgThree
+          img: require('@/assets/illustrations/about/Salmo goals 3.svg')
         }
       ]
     }
@@ -83,30 +59,6 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/assets/styles/variables.scss';
-
-#about {
-  padding: $spacing*10 0 0;
-}
-
-.logo-about {
-  display: flex;
-  align-items: center;
-
-  img {
-    width: 12vw;
-    height: 12vw;
-    margin-right: 2vw;
-  }
-
-  h1 {
-    font-size: 12vw;
-  }
-}
-
-h2 {
-  font-size: 7vw;
-  margin-top: 5vw;
-}
 
 .help {
   background: $background;
