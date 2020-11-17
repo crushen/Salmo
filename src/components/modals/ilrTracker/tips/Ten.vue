12fad661c39faa3c85f0a2814dbfbbb7aa6d503c
Charlotte Rushen <charlotterushen@hotmail.com>
2020-11-17 11:10:52 +0000

    Added spacing to 10 year plan tips

--- src/components/modals/ilrTracker/tips/Ten.vue
+++ src/components/modals/ilrTracker/tips/Ten.vue
@@ -2,22 +2,22 @@
   <base-tip @closeModal="closeModal">
     <h2>The Golden ILR Rules</h2>
 
-    <ol>
+    <ol class="margin-s top bottom">
       <li>
         <b>You cannot leave the UK for more than 180 whole days in a single trip.</b>
         <p>One whole day counts as 24 hours, when we calculate your travel log, we do not count the half days (such as departure or arrival days) and only the whole days.</p>
       </li>
 
-      <li>
+      <li class="margin-s top">
         <b>You cannot leave for more than 540 days in total within 10 years period.</b>
       </li>
 
-      <li>
+      <li class="margin-s top">
         <b>You maintain continuous legal leave in the UK for 10 years.</b>
         <p>The word “leave” means the duration you can stay in the UK during the validity of you visa. </p>
       </li>
 
-      <li>
+      <li class="margin-s top">
         <b>If your current visa is expiring and you are required to make an application from your home country, you must leave the UK before your visa’s expiry date and re-enter to the UK with the new visa within 180 days from the day you depart.</b>
         <p>Example: Re-entry includes returning into the UK as a visitor whether you need a visitor visa or not.</p>
       </li>
@@ -43,4 +43,7 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/styles/variables.scss';
 
+ol {
+  list-style-type: decimal;
+}
 </style>
