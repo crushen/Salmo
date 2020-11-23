0faa2215b23c5672d2f257e27b99e3fc6e974e7e
Charlotte Rushen <charlotterushen@hotmail.com>
2020-11-23 13:44:02 +0000

    Removed the minus 2 days - sort this in computed

--- src/components/modals/ilrTracker/red/EditHoliday.vue
+++ src/components/modals/ilrTracker/red/EditHoliday.vue
@@ -111,7 +111,6 @@ export default {
       const dt1 = new Date(start),
             dt2 = new Date(end);
 
-      // -2 to take off the two travel days
       return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24)) - 2
     },
     // Validations
