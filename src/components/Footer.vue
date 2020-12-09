81da3a4f28e8805af883ccc86ad7d715f295b860
Charlotte Rushen <charlotterushen@hotmail.com>
2020-12-09 15:08:33 +0000

    Made footer a component

---
+++ src/components/Footer.vue
@@ -0,0 +1,61 @@
+<template>
+  <footer class="margin-l top">
+    <div class="content">
+      <img src="@/assets/logo/colour.svg" alt="" class="img">
+      
+      <ul>
+        <li>
+          <router-link :to="{ name: 'home' }">
+            Home
+          </router-link>
+        </li>
+
+        <li>
+          <router-link :to="{ name: 'about' }">
+            About
+          </router-link>
+        </li>
+
+        <li class="margin-s top">
+          <router-link :to="{ name: 'terms' }">
+            Terms and Conditions
+          </router-link>
+        </li>
+
+        <li>
+          <router-link :to="{ name: 'privacy' }">
+            Privacy Policy
+          </router-link>
+        </li>
+      </ul>
+
+      <p class="margin-s top">© 2020 Salmo Visa Limited. All Rights Reserved.</p>
+
+      <p>Salmo Visa Limited is a company registered in England and Wales (No. 12482305).</p>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+
+footer {
+  background: $blue;
+  padding: 60px 0 80px 0;
+  position: relative;
+
+  img {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 60px;
+  }
+
+  a,
+  p {
+    font-size: 14px;
+    color: white;
+    font-weight: 300;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -92,41 +92,7 @@
       </div>
     </main>
 
-    <footer class="margin-l top">
-      <div class="content">
-        <img src="@/assets/logo/colour.svg" alt="" class="img">
-        
-        <ul>
-          <li>
-            <router-link :to="{ name: 'home' }">
-              Home
-            </router-link>
-          </li>
-
-          <li>
-            <router-link :to="{ name: 'about' }">
-              About
-            </router-link>
-          </li>
-
-          <li class="margin-s top">
-            <router-link :to="{ name: 'terms' }">
-              Terms and Conditions
-            </router-link>
-          </li>
-
-          <li>
-            <router-link :to="{ name: 'privacy' }">
-              Privacy Policy
-            </router-link>
-          </li>
-        </ul>
-
-        <p class="margin-s top">© 2020 Salmo Visa Limited. All Rights Reserved.</p>
-
-        <p>Salmo Visa Limited is a company registered in England and Wales (No. 12482305).</p>
-      </div>
-    </footer>
+    <app-footer />
   </div>
 </template>
 
@@ -134,9 +100,10 @@
 import patreonMessage from '@/components/PatreonMessage'
 import loggedOutAlert from '@/components/alerts/LoggedOut'
 import deletedAccount from '@/components/alerts/DeletedAccount'
+import appFooter from '@/components/Footer'
 
 export default {
-  components: { patreonMessage, loggedOutAlert, deletedAccount },
+  components: { patreonMessage, loggedOutAlert, deletedAccount, appFooter },
   data() {
     return {
       loggedOut: false,
@@ -305,25 +272,7 @@ p {
   }
 }
 
-footer {
-  background: $blue;
-  padding: 60px 0 80px 0;
-  position: relative;
 
-  img {
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: 60px;
-  }
-
-  a,
-  p {
-    font-size: 14px;
-    color: white;
-    font-weight: 300;
-  }
-}
 
 @media screen and (min-width: 375px) {
   header {
