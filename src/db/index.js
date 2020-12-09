4a4bcf75232b2eaed3de1006d46a4f7e70732fcd
Charlotte Rushen <charlotterushen@hotmail.com>
2020-12-09 17:20:12 +0000

    Add db to git

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+export const db = firebase
+.initializeApp({
+  apiKey: `${process.env.VUE_APP_API_KEY}`,
+  authDomain: `${process.env.VUE_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.VUE_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.VUE_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.VUE_APP_MESSAGE_SENDER_ID}`,
+  appId: `${process.env.VUE_APP_ID}`,
+  measurementId: `${process.env.VUE_MEASUREMENT_ID}`
+})
+.firestore()
